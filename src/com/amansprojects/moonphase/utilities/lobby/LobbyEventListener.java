720cc8b60bbbb93fce3fd9b27377182b45245a11
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2022-04-03 15:29:23 +0100

    Prevent fall damage in lobbies

--- src/com/amansprojects/moonphase/utilities/lobby/LobbyEventListener.java
+++ src/com/amansprojects/moonphase/utilities/lobby/LobbyEventListener.java
@@ -1,9 +1,9 @@
 package com.amansprojects.moonphase.utilities.lobby;
 
-import net.gcnt.additionsplus.AdditionsPlus;
 import net.gcnt.additionsplus.api.AdditionsAPI;
-import net.gcnt.additionsplus.files.books.Book;
-import net.gcnt.additionsplus.files.items.CustomItem;
+import net.gcnt.additionsplus.api.AdditionsPlugin;
+import net.gcnt.additionsplus.api.objects.AdditionsBook;
+import net.gcnt.additionsplus.api.objects.AdditionsItem;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
@@ -11,6 +11,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.potion.PotionEffect;
@@ -18,13 +19,13 @@ import org.bukkit.potion.PotionEffectType;
 
 public class LobbyEventListener implements Listener {
 
-    AdditionsAPI additionsAPI = AdditionsPlus.getAPI();
+    AdditionsAPI additionsAPI = ((AdditionsPlugin) Bukkit.getPluginManager().getPlugin("Additions")).getAPI();
 
 	public void sendPlayerToLobby(Player player) {
         player.getInventory().clear();
         player.getInventory().setArmorContents(null);
-        Book helpBook = additionsAPI.getBookByName("Help");
-        CustomItem gameSelectorItem = additionsAPI.getItemByName("Games");
+        AdditionsBook helpBook = additionsAPI.getBookByName("Help");
+        AdditionsItem gameSelectorItem = additionsAPI.getItemByName("Games");
         player.setGameMode(GameMode.ADVENTURE);
 		player.teleport(new Location(Bukkit.getWorld("Lobby"), 0, 7, 0));
         for (PotionEffect effect : player.getActivePotionEffects()) { player.removePotionEffect(effect.getType()); }
@@ -47,6 +48,19 @@ public class LobbyEventListener implements Listener {
         if (event.getPlayer().getLocation().getWorld().toString().equalsIgnoreCase("Lobby")) {
         	Player player = event.getPlayer();
             sendPlayerToLobby(player);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+        String worldName = event.getEntity().getWorld().getName();
+        if (worldName.equalsIgnoreCase("Lobby") || worldName.equalsIgnoreCase("SkyWars")) {
+            if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+                event.setCancelled(true);
+            }
         }
     }
 }
